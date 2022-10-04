2fadd3c197aecc092383cd9bae399a029f2bd896
benpapac <ben.papac@gmail.com>
2022-10-04 11:44:47 -0700

    add substring permutation in AlgoI

---
+++ AlgoI/permutationsOfSubString.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+
+// This algorithm relies on a "sliding window" solution. See:
+// https://www.youtube.com/watch?v=6gRj_FH3MsA
+//  https://stackoverflow.com/questions/8269916/what-is-sliding-window-algorithm-examples
+
+
+// solution after reviewing youtube and solution samples.
+var checkInclusion = function (s1, s2) {
+	let x = s1.length,
+		y = s2.length;
+	let a = 'a'.charCodeAt(0);
+
+	if (!y || y < x) return false;
+	if (!x) return true;
+
+	let characters = new Array(26).fill(0);
+
+	for (let i = 0; i < s1.length; i++) {
+		characters[s1.charCodeAt(i) - a]--;
+	}
+
+	for (let i = 0; i < y; i++) {
+		characters[s2.charCodeAt(i) - a]++;
+
+		if (i < x - 1) continue;
+		if (i > x - 1) characters[s2.charCodeAt(i - x) - a]--;
+		if (characters.every((val) => val === 0)) return true;
+	}
+
+	return false;
+};
